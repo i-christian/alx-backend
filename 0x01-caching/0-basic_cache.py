52c0cfbf144fdb190fb95f83cb3ffad5c5eee2f0
Christian <111076892+i-christian@users.noreply.github.com>
2024-04-03 23:51:35 +0200

    update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,12 +10,9 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
